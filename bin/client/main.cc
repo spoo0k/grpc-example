1af5b50e1e076537692dd2c2876d3a4445015ea7
spoo0k <mukhinva2@gmail.com>
2024-05-21 16:22:49 +0300

    gg

--- bin/client/main.cc
+++ bin/client/main.cc
@@ -3,11 +3,12 @@
 //
 
 #include  <iostream>
+#include <thread>
 #include <xrpc-test/client/x-client.h>
 #include <xrpc-test/protobufs/math.grpc.pb.h>
 
 int main() {
-  xrpc::XClient client("10.0.22.139", 50051);
+  xrpc::XClient client("127.0.0.1", 50051);
   xrpc::math::MathRequest request;
   request.set_operation(xrpc::math::MathOperation::Multiply);
   request.mutable_arguments()->set_lhs(5.0);
--- bin/server/main.cc
+++ bin/server/main.cc
@@ -3,31 +3,50 @@
 //
 
 #include <iostream>
+#include <thread>
 #include <xrpc-test/server/x-server.h>
 #include <xrpc-test/protobufs/math.grpc.pb.h>
 
 
-
-int main() {
-  xrpc::XServer server([](const xrpc::math::MathRequest& request) -> std::optional<float> {
-    if (request.operation() == xrpc::math::MathOperation::Add) {
+int main()
+{
+  xrpc::XServer server([](const xrpc::math::MathRequest& request) -> std::optional<float>
+  {
+    if (request.operation() == xrpc::math::MathOperation::Add)
+    {
       return request.arguments().lhs() + request.arguments().rhs();
     }
-    else if (request.operation() == xrpc::math::MathOperation::Subtract) {
+    else if (request.operation() == xrpc::math::MathOperation::Subtract)
+    {
       return request.arguments().lhs() - request.arguments().rhs();
     }
-    else if (request.operation() == xrpc::math::MathOperation::Multiply) {
+    else if (request.operation() == xrpc::math::MathOperation::Multiply)
+    {
       return request.arguments().lhs() * request.arguments().rhs();
     }
-    else if (request.operation() == xrpc::math::MathOperation::Divide) {
-      if (request.arguments().rhs() == 0) {
+    else if (request.operation() == xrpc::math::MathOperation::Divide)
+    {
+      if (request.arguments().rhs() == 0)
+      {
         return std::nullopt;
       }
       return request.arguments().lhs() / request.arguments().rhs();
     }
     return std::nullopt;
   });
-  server.run("0.0.0.0", 50051);
-  std::cout << "Hello, World!" << std::endl;
+
+  std::thread t([&server]()
+  {
+    server.run("0.0.0.0", 50051);
+  });
+  t.detach();
+
+  while (true)
+  {
+    std::cout << "Hello, World!" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+  // server.run("0.0.0.0", 50051);
+  // std::cout << "Hello, World!" << std::endl;
   return 0;
 }
